3051fd2b406463b85f047b8dd17c303bba70ed2c
chandlerurenda <chandler.urenda@gmail.com>
2026-09-25 15:25:19 -0700

    wrote JS for email correspondence

---
+++ app.js
@@ -0,0 +1,26 @@
+//
+//
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_e70k63s",
+      "template_avs8xo8",
+      event.target,
+      "24pPWWJ_dSeKPEGlj",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on chandler.urenda@gmail.com.",
+      );
+    });
+}
